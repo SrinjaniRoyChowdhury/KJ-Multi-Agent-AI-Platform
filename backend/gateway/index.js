36ce6216cd954aa83cd049bde2121a6043d0ecab
Srinjani Roy Chowdhury <roysrinjani04@gmail.com>
2026-09-16 04:39:09 +0530

    initial folders, gateway setup

---
+++ backend/gateway/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const PORT = process.env.PORT
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.json({message: "Hello from Gateway!"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
